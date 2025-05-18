75d9984ed7102a628d08396ac026c7e712180004
Marcos Duran <Marcos_Duran@ucne.edu.do>
2025-05-18 17:46:18 -0400

    Initial commit

---
+++ app/src/main/java/edu/ucne/registroticket/Data/Local/Database/ticketDb.kt
@@ -0,0 +1,17 @@
+package edu.ucne.registroticket.Data.Local.Database
+
+import androidx.room.Database
+import androidx.room.RoomDatabase
+import edu.ucne.registroticket.Data.Local.Dao.TicketDao
+import edu.ucne.registroticket.Data.Local.Entities.TicketEntity
+
+@Database(
+    entities = [
+        TicketEntity::class
+    ],
+    version = 1,
+    exportSchema = false
+)
+abstract class TicketDb : RoomDatabase(){
+    abstract fun ticketDao(): TicketDao
+}
